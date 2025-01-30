eda486e9285ea34a292a7f05a7f2a1da8d0124fc
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-01-30 19:10:46 +0530

    Usage of router in main.js

--- url-shortener-frontend/src/main.js
+++ url-shortener-frontend/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);    // Use the router
+app.mount('#app');  // Mount the app to the DOM
