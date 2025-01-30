ac4fa07bab455047b87e82bf057c31fb263ebb5e
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-01-30 19:22:20 +0530

    Implemented logClickevent to log click event into clicks table using ip address and user agent of the user and formatDate helper functions

--- url-shortener-backend/utils.go
+++ url-shortener-backend/utils.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"log"
 	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
 func enableCORS(w http.ResponseWriter) {
@@ -26,4 +28,26 @@ func getEnv(key string, defaultValue string) string {
 		return value
 	}
 	return defaultValue
+}
+
+// Function to log click events
+func logClickEvent(shortURL string, r *http.Request) {
+
+	ipAddress := r.RemoteAddr  // Get IP address of the requester
+	userAgent := r.UserAgent() // Get user agent string
+
+	// Use urlShortener.db to access the database
+	_, err := urlShortener.db.Exec("INSERT INTO clicks (short_url, ip_address, user_agent) VALUES (?, ?, ?)", shortURL, ipAddress, userAgent)
+	if err != nil {
+		log.Printf("Error logging click event: %v", err)
+	}
+}
+
+// Helper function to format date from string to desired format.
+func formatDate(expirationDate string) (string, error) {
+	parsedTime, err := time.Parse(time.RFC3339, expirationDate)
+	if err != nil {
+		return "", err
+	}
+	return parsedTime.Format("02 Jan 2006"), nil
 }
