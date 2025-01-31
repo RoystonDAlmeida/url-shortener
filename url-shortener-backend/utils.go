e8ad4b80ad996a0a91d78318d0ae19f1986e793b
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-02-01 03:19:06 +0530

    Added DELETE option in CORS headers

--- url-shortener-backend/utils.go
+++ url-shortener-backend/utils.go
@@ -10,7 +10,7 @@ import (
 
 func enableCORS(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*") // Allows all origins
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
 
