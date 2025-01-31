e622e9e49d04a7852183fa62313c7dd6822170c8
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-02-01 03:20:47 +0530

    Added Dashboard button

--- url-shortener-frontend/src/App.vue
+++ url-shortener-frontend/src/App.vue
@@ -3,6 +3,9 @@
     <div class="header">
       <img src="@/assets/url_shortener_logo.jpeg" alt="URL Shortener Logo" class="logo" />
       <h1>URL Shortener</h1>
+      <router-link to="/dashboard">
+        <button class="dashboard-button">Dashboard</button>
+      </router-link>
     </div>
     <router-view></router-view> <!-- Render components here depending on their route -->
   </div>
